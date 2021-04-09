5bcd696395fe80e2e436d92685b90aa1ec9ac324
Koray <koray.akyurek6@gmail.com>
2021-04-09 10:27:38 +0200

    Initial commit

---
+++ src/main/java/testMaven/Principale.java
@@ -0,0 +1,25 @@
+package testMaven;
+
+import com.google.gson.Gson;
+
+public class Principale {
+	
+	private static Gson newGson() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public static void main(String[] args) {
+		// TODOAuto-generated methodstub
+		Gson gson= newGson();
+		System.out.println(gson.toJson("Hello World !"));
+		
+	}
+
+
+
+	
+	
+
+}
